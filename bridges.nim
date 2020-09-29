553567e6b5d7a8ca0cdd3e0404c429596719ef7f
Rosen Popov <rpopov@qti.qualcomm.com>
2020-09-29 18:54:55 +0300

    Added a locks.nim file for future implemenetation of file locks

--- bridges.nim
+++ bridges.nim
@@ -8,33 +8,73 @@ type
   bridge = object
     source: string
     location: string
-    name: string
+    link_name: string
 
 proc Bridge (init:seq[string]):bridge = 
   if init.len == 3:
-    result = bridge(source : init[0],location:init[1],name:init[2])
+     return  bridge(source : init[0],location:init[1],link_name:init[2])
+  raiseAssert (fmt"Incorrect entry in csv {init}") 
 
 proc BuildBridge(arg:bridge): string = 
   let delim = ","
-  result = arg.source & delim & arg.location & delim & arg.name
+  result = arg.source & delim & arg.location & delim & arg.link_name
   result = result & ";"
   return result
 
+proc BuildBridge(source,location,name:string): string = 
+  let delim = ","
+  result = source & delim & location & delim & name
+  result = result & ";"
+  return result
+
+proc BuildBridge(arg:seq[string]): string = 
+  let delim = ","
+  result ="" 
+  if arg.len >= 3:
+    result = arg[0] & delim & arg[1] & delim & arg[2] & ";"
+  return result
+
 proc ShowReffsTo(source,sfile_name:string):seq[bridge] =
-  var tab : seq[bridge]
-  var file_stream = newFileStream(sfile_name, fmRead)  
+  #TODO add a standard lock(file) checker  
+  var 
+    tab : seq[bridge]
+    file_stream = newFileStream(sfile_name, fmRead)  
+
   if file_stream == nil:
     raiseAssert("Could not open file " & sfile_name)
   var Reader: CsvParser
   open(Reader, file_stream,sfile_name,separator=',')
   while readRow(Reader):
-    if sfile_name == Reader.row[0]:
+    if source == Reader.row[0]:
+      tab.add(Bridge(Reader.row))
+    elif source == "":
       tab.add(Bridge(Reader.row))
   close(file_stream)
   return tab
 
-proc AddRef(file,link_bridge,source,sfile_name:string):bool =
-   
+# TODO make it so that thare is a bitset of override parameters 
+# instead of just a bool that is passed as a parameter
+
+proc AddRef(main_warehouse,source,location,name,sfile_name:string, override:bool = false):bool =
+  #TODO add a standard lock(file) checker  
+  let 
+    link_src = main_warehouse & "/" & source
+    link_dst = location & "/" & name
+  var
+    entry :string = BuildBridge(link_src,location,name)
+    sfile_file :File = open(sfile_name,fmAppend)
+  if sfile_file == nil:
+    raiseAssert(fmt"Could no open file for writing {sfile_name}")
+  if symlinkExists(link_dst):
+    if override == false:
+      raiseAssert(fmt"Link {link_dst} already exists")
+    else :
+      
+  try:
+    createSymlink(link_src,link_dst)
+  except OsError:
+    raiseAssert(fmt"Could not create a link between {link_src} and {link_dst}")
+  if 
   return true
 
 proc CheckBroken(file_stream:File): seq[bridge] = 
