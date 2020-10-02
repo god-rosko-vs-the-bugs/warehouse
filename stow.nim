e13c8a81751351fed296e17460986012dd4b619a
Rosen Popov <rdonkow@gmail.com>
2020-10-03 01:34:45 +0300

    Created functionality to send file to warehouse and leave a symlink

--- stow.nim
+++ stow.nim
@@ -1,22 +1,32 @@
 import os
 import strformat
 
-proc Get
-
 proc SendToWarehouse*(filename,warehouse:string,remnant_link:bool=false):bool=
-  var FileInformation = getFileInfo(filename)
   var cut_name:string
   if filename[filename.len-1] == '/':
-    
+    cut_name = filename[0..<filename.len-1]
+  else:
+    cut_name = filename
+
+  var FileInformation = getFileInfo(cut_name)
+  var (path,name,ext) = splitFile(cut_name)   
+  var target_name:string = name & ext
   case FileInformation.kind:
     of pcDir:
-
-      echo "is dir"
+      if dirExists(warehouse & target_name):
+        stderr.write (fmt"Directory {target_name} exist in warehouse")
+        raiseAssert(fmt"Directory {target_name} exist in warehouse")
+      else:
+        moveFile(filename,warehouse & target_name)
+        if remnant_link:
+          createSymlink(warehouse & target_name,filename)
     of pcFile:
-      echo "is file"
-    else: 
-      stderr.write( fmt"Cannot store {filename} : is already a link! ")
-
-
-
-
+      if fileExists (warehouse & target_name):
+        stderr.write (fmt"File {target_name} exist in warehouse")
+        raiseAssert (fmt"File {target_name} exist in warehouse")
+      else:
+        moveFile(filename,warehouse & target_name)
+        if remnant_link:
+          createSymlink(warehouse & target_name,filename)
+    else:
+      stderr.write( fmt"Cannot store {filename} : is already a link! -> " & expandSymlink(filename))
